443c0ab033e1d8cace56cf2011ebb29086b6c8f8
Colin Kennedy <colin@mrkennedy.ca>
2024-12-04 11:08:59 -0400

    preempt check for gitignore file

--- walkrepo.go
+++ walkrepo.go
@@ -14,7 +14,6 @@ func WalkRepo(root string, walkFn filepath.WalkFunc) error {
 	var ps []gitignore.Pattern
 	domain := []string{}
 
-	// Recursive function to walk through directories
 	var walk func(string, []string) error
 	walk = func(path string, domain []string) error {
 		f, err := os.Open(path)
@@ -28,29 +27,34 @@ func WalkRepo(root string, walkFn filepath.WalkFunc) error {
 			return err
 		}
 
+		// First, check for .gitignore in this directory and process it
 		for _, file := range files {
-			filePath := filepath.Join(path, file.Name())
-
-			// Check if the file is a .gitignore file
 			if file.Name() == ".gitignore" {
+				filePath := filepath.Join(path, file.Name())
 				filePatterns, err := parseFilePatterns(filePath, domain)
-
 				if err != nil {
 					return err
 				}
-
 				ps = append(ps, filePatterns...)
 			}
+		}
 
+		// Then process all other files
+		for _, file := range files {
+			if file.Name() == ".gitignore" {
+				continue // Skip .gitignore files as we've already processed them
+			}
+
+			filePath := filepath.Join(path, file.Name())
 			matcher := gitignore.NewMatcher(ps)
+
 			if !matcher.Match(domain, file.IsDir()) {
 				err := walkFn(filePath, file, nil)
 				if err != nil {
 					if err == filepath.SkipDir && file.IsDir() {
 						continue
-					} else {
-						return err
 					}
+					return err
 				}
 
 				if file.IsDir() {
